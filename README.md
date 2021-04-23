661b7b416760fe12d8e8324a71b62283353ee24c
Bu9 <s1011550@gmail.com>
2021-04-23 17:47:57 +0800

    整理、排版一下，移除沒再用的

--- README.md
+++ README.md
@@ -1,46 +1,47 @@
 # 使用方式
 
-先確定Windows Update都更新完，不然裝SSMS會失敗
+先確定 Windows Update 都更新完，不然裝 SSMS 會失敗
 
-以工作管理員開啟Cmd
+以工作管理員開啟 Cmd
 
-輸入 PowerShell.exe -ExecutionPolicy UnRestricted -File {Refill.ps1的實體路徑}
-
+輸入 PowerShell.exe -ExecutionPolicy UnRestricted -File {Refill.ps1 的實體路徑}
 
 ## 安裝程式清單
+
 ```
+
 Google Chrome
 Line
-SSMS
-Postman
-Fiddler
-WinScp - FTP
 TeamViewer
-Visual Studio Code
-Visual Studio 2019 - Community
-SourceTree
 Telegram
 7zip
+
 AdobeReader(PDF)
-Google Backup And Sync (Google Drive)
-Git
-Nodejs
-LBRY
-Steam
 libreoffice
-Spotify
 Discord
+Postman
+Visual Studio Code
+
+Nodejs
+Git
+SourceTree
+Visual Studio 2019 - Community
+SSMS
+
 ```
 
 ## 選擇安裝
 
 #### Vue.js
+
 ```
 npm install -g vue
 npm install -g @vue/cli
 npm install -g @vue/cli-service-global
 ```
+
 ### VSCode
+
 ```
 同步設定
 code --install-extension "shan.code-settings-sync"
--- Refill.ps1
+++ Refill.ps1
@@ -1,26 +1,28 @@
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 choco install googlechrome -y
 choco install line -y
 choco install teamviewer -y
 choco install telegram -y
 choco install 7zip -y
+
 choco install adobereader -y
 choco install libreoffice-fresh -y
 choco install discord -y
-
-choco install winscp -y
 choco install postman -y
 choco install vscode -y
-choco install fiddler -y
+
 choco install nodejs -y
 choco install git -y
 choco install sourcetree -y
 choco install visualstudio2019community -y
 choco install visualstudio2019-workload-netweb -y
+
 choco install visualstudio2019-workload-azure -y
 choco install visualstudio2019-workload-visualstudioextension -y
 choco install visualstudio2019-workload-data -y
 choco install visualstudio2019-workload-netcoretools -y
 choco install visualstudio2019-workload-manageddesktop -y
+
 choco install sql-server-management-studio -y
