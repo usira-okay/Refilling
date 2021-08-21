075b6e51b786a7f3dda7c0de0931800f572a81f0
suxiaobu9 <37999690+suxiaobu9@users.noreply.github.com>
2021-08-21 15:41:02 +0800

    增加輸入法

--- Refill.ps1
+++ Refill.ps1
@@ -1,3 +1,8 @@
+$UserLanguageList = New-WinUserLanguageList -Language "zh-TW"
+$UserLanguageList.Add("en-US")
+Set-WinUserLanguageList -LanguageList $UserLanguageList -Force
+Set-WinDefaultInputMethodOverride -InputTip "0409:00000409"
+
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
