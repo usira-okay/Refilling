b96399b603705c2d1f94e2c243bc2b219669c620
ari <ari@miniasp.com>
2021-08-29 14:53:05 +0800

    test

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,5 @@
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
 Restart-Computer
