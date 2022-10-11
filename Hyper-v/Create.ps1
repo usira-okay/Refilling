24201a80e54379ec98d8af2513164dc45b7b54af
ari <ari@miniasp.com>
2022-10-11 15:57:33 +0800

    update

--- Hyper-v/Create.ps1
+++ Hyper-v/Create.ps1
@@ -17,10 +17,14 @@ if (Test-Administrator) {
     $memory = 2
     $CPUCount = 10
 
-    # 儲存建立的 VM 物件，可以方便後續進行設定
-    $VM = New-VM -Name $VMName -SwitchName $SWName -Path $VMPath `
+    $VM = New-VM -Name $VMName `
+        -SwitchName $SWName `
+        -Path $VMPath `
         -NewVHDPath "${VMPath}\${VMName}\Virtual Hard Disks\${VMName}.vhdx" `
-        -MemoryStartupBytes ([int64]$memory * $GiB) -Generation 1 -NewVHDSizeBytes ([int64]$disk * $GiB)
+        -MemoryStartupBytes ([int64]$memory * $GiB) `
+        -Generation 1 `
+        -NewVHDSizeBytes ([int64]$disk * $GiB)
+
     $VM | Set-VMProcessor -Count $CPUCount
     $VM | Set-VMProcessor -ExposeVirtualizationExtensions $true
     $VM | Set-VMMemory -DynamicMemoryEnabled $true
