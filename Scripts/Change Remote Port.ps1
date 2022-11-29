869fa319452d2f32a1c8a2356294fc4259475ba1
Ari <arisuokayokay@gmail.com>
2022-11-29 20:18:05 +0800

    增加打開遠端桌面功能的語法

--- Scripts/Change Remote Port.ps1
+++ Scripts/Change Remote Port.ps1
@@ -5,3 +5,4 @@ $portvalue = 3390
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name 'PortNumber' -Value $portvalue 
 New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
 New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $portvalue
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
