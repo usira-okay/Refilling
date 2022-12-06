20a66987437f4e6deaedc5154fd7d28d522a750c
Ari <arisuokayokay@gmail.com>
2022-12-06 21:32:06 +0800

    增加註解

--- Scripts/Change Remote Port.ps1
+++ Scripts/Change Remote Port.ps1
@@ -2,7 +2,10 @@ Write-Host 'Change Remote Port'
 
 $portvalue = 3390
 
+# 變更遠端桌面的 Port 以及變更遠端桌面的 Port
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name 'PortNumber' -Value $portvalue 
 New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
 New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $portvalue
+
+# 開啟遠端桌面功能
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
