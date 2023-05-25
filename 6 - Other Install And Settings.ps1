b9d865284e95c43c38228faf08ea968cfe47a2ce
AriSu <arisu@tasys.io>
2023-05-25 20:04:24 +0800

    update

--- 6 - Other Install And Settings.ps1
+++ 6 - Other Install And Settings.ps1
@@ -4,17 +4,6 @@ Set-Location ([System.IO.Path]::GetDirectoryName($PSCommandPath))
 
 Write-Host 'Other install and settings'
 
-if (!($env:Path.Contains('C:\Program Files\Git\bin'))) {
-    $env:Path
-    
-    if (!$env:Path.EndsWith(';')) {
-        $env:Path = $env:Path + ';'
-    }
-    $env:Path = $env:Path + "C:\Users\$env:USERNAME\scoop\apps\git\current\bin"
-
-    $env:Path
-}
-
 Write-Host 'Install AzureCLI'
 # Install AzureCLI
 $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
