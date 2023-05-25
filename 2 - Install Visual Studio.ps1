ecb6d93724964a8054abc1fe01a750e0a2aba0b4
AriSu <arisu@tasys.io>
2023-05-25 17:22:50 +0800

    update

---
+++ 2 - Install Visual Studio.ps1
@@ -0,0 +1,10 @@
+Set-Location ([System.IO.Path]::GetDirectoryName($PSCommandPath))
+
+. .\Test-Admin.ps1 -p $PSCommandPath
+
+Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vs_community.exe -OutFile .\vs_community.exe
+
+$process = Start-Process -FilePath vs_community.exe -ArgumentList "--allWorkloads", "--passive", "--installWhileDownloading", "--locale en-US", "--wait" -Wait -PassThru
+Write-Output $process.ExitCode 
+
+pause
