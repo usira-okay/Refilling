eb6d9f7d1d929676fd050673ba955316ba0d9c8b
Ari <arisuokayokay@gmail.com>
2024-01-06 10:25:54 +0800

    update

--- 2 - Visual Studio.ps1
+++ 2 - Visual Studio.ps1
@@ -2,10 +2,17 @@ Set-Location ([System.IO.Path]::GetDirectoryName($PSCommandPath))
 
 . .\Test-Admin.ps1 -p $PSCommandPath
 
-Write-Host 'Visual Studio'
-Invoke-WebRequest -Uri https://aka.ms/vs/17/release/vs_community.exe -OutFile .\vs_community.exe
 
-$process = Start-Process -FilePath vs_community.exe -ArgumentList "--allWorkloads", "--passive", "--installWhileDownloading", "--locale en-US", "--wait" -Wait -PassThru
-Write-Output $process.ExitCode 
+
+Write-Host 'Visual Studio'
+
+$vsUrls = 'https://aka.ms/vs/17/release/vs_community.exe', 'https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Preview&version=VS2022&source=VSLandingPage&cid=2060';
+''
+foreach ($url in $vsUrls) {
+    Invoke-WebRequest -Uri $url -OutFile .\installer.exe
+    
+    $process = Start-Process -FilePath installer.exe -ArgumentList "--allWorkloads", "--passive", "--installWhileDownloading", "--locale en-US", "--wait" -Wait -PassThru
+    Write-Output $process.ExitCode 
+}
 
 Pause
