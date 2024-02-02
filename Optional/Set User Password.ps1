0afaf0a8b99317985c39bbecfc5a5dbe188ce8ff
arisu <arisu@91app.com>
2024-02-02 23:12:13 +0800

    update

--- Optional/Set User Password.ps1
+++ Optional/Set User Password.ps1
@@ -4,8 +4,25 @@ Set-Location ([System.IO.Path]::GetDirectoryName($PSCommandPath))
 
 Write-Host 'Set User Password'
 
-$newpassword = ConvertTo-SecureString "YourNewPassword" -AsPlainText -Force
-$user = Get-LocalUser -Name $env:USERNAME
-$user | Set-LocalUser -Password $newpassword
+# 要求使用者第一次輸入密碼
+$password1 = Read-Host "Enter password" -AsSecureString
+
+# 要求使用者第二次輸入密碼
+$password2 = Read-Host "Enter password again" -AsSecureString
+
+# 將 SecureString 轉換為普通字串以便比較
+$ptr1 = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password1)
+$plaintextPassword1 = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($ptr1)
+
+$ptr2 = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password2)
+$plaintextPassword2 = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($ptr2)
+
+# 比較兩次輸入的密碼
+if ($plaintextPassword1 -eq $plaintextPassword2) {
+    Write-Host "Password match."
+    $user = Get-LocalUser -Name $env:USERNAME
+    $user | Set-LocalUser -Password $password1
+}
+
 
 Pause
