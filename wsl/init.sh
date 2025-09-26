27919e30ba4cd20963080d9a6d088fc41aa8c703
usira-okay <arisuokayokay@gmail.com>
2025-09-26 13:46:13 +0800

    Update init.sh

--- wsl/init.sh
+++ wsl/init.sh
@@ -91,5 +91,6 @@ sudo apt update && sudo apt upgrade -y
 
 npm install -g @google/gemini-cli
 npm install -g @anthropic-ai/claude-code
+npm install -g @openai/codex
+npm install -g @github/copilot
 
-claude
