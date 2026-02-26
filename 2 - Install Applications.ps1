d59596ed1c082b059a2be67f4088fb32a7b8d819
usira-okay <arisuokayokay@gmail.com>
2026-02-26 23:51:08 +0800

    update

--- 2 - Install Applications.ps1
+++ 2 - Install Applications.ps1
@@ -4,16 +4,11 @@ Set-Location ([System.IO.Path]::GetDirectoryName($PSCommandPath))
 
 Write-Host 'Install Applications by Winget'
 
-Write-Information "Downloading WinGet and its dependencies..."
-$location = Get-Location
 $webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile('https://aka.ms/getwinget', "$location\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
-$webClient.DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', "$location\Microsoft.VCLibs.x64.14.00.Desktop.appx")
-$webClient.DownloadFile('https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx', "$location\Microsoft.UI.Xaml.2.7.x64.appx")
-
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+$webClient.DownloadFile('https://aka.ms/getwinget', "$env:TEMP\winget.msixbundle")
+$webClient.DownloadFile('https://aka.ms/windowsappsdk/1.8/latest/windowsappruntimeinstall-x64.exe', "$env:TEMP\WinAppRuntime.exe")
+Start-Process -FilePath "$env:TEMP\WinAppRuntime.exe" -ArgumentList '--quiet' -Wait
+Add-AppxPackage -Path "$env:TEMP\winget.msixbundle"
 
 winget import -i '.\Winget Package\Tools.json' --accept-package-agreements --accept-source-agreements
 
